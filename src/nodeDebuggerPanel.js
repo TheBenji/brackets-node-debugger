c674c0a15472b7bbbad463474ecbe0764c46653f
B. August <benji889@yahoo.de>
2014-07-19 20:02:49 +0100

    history for evaluation

--- src/nodeDebuggerPanel.js
+++ src/nodeDebuggerPanel.js
@@ -3,6 +3,8 @@ define(function (require, exports) {
 
     var _nodeDebuggerDomain,
         _panel,
+        history = [],
+        historyCurrent = 0,
         $debuggerContent = $(null),
         $debuggerInput = $(null);
 
@@ -26,16 +28,39 @@ function log($msg) {
         $debuggerContent = _panel.find('#brackets-node-debugger-content');
         $debuggerInput = _panel.find('#brackets-node-debugger-input');
 
-		$debuggerInput.on('keypress', function(e) {
+		$debuggerInput.on('keydown', function(e) {
+            //On enter send command
             if(e.keyCode == 13) {
                 var com = $debuggerInput.html();
 
                 if(com.length > 0) {
+                    history.push(com);
+                    historyCurrent = history.length;
                     log( $('<span>').text('>> ' + com) );
                     nodeDebuggerDomain.exec('eval', com);
                     //reset the input field
                     $debuggerInput.html('');
                 }
+            }
+            //On key up/down scroll through history
+            if(e.keyCode == 40) {
+                historyCurrent++;
+                if(history[historyCurrent]) {
+                    $debuggerInput.html( history[historyCurrent] );
+                } else {
+                    historyCurrent = history.length;
+                    $debuggerInput.html('');
+                }
+                //e.preventDefault();
+            }
+            if(e.keyCode == 38) {
+                historyCurrent--;
+                if(history[historyCurrent]) {
+                    $debuggerInput.html( history[historyCurrent] );
+                } else {
+                    historyCurrent = 0;
+                }
+                e.preventDefault();
             }
 		});
     }
