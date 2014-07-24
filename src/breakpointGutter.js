86bacb801bc66d9e072be1e58598cd0c653b35e1
Benjamin August <benji889@yahoo.de>
2014-07-24 08:02:13 +0100

    actual remove bp on remove all & some indentation fixes -.-

--- src/breakpointGutter.js
+++ src/breakpointGutter.js
@@ -7,50 +7,50 @@
 
 /*global define, brackets, $ */
 define(function (require, exports) {
-    "use strict";
+	"use strict";
 
 	var _ = brackets.getModule("thirdparty/lodash"),
-    EditorManager = brackets.getModule("editor/EditorManager"),
-        DocumentManager = brackets.getModule("document/DocumentManager");
+	EditorManager = brackets.getModule("editor/EditorManager"),
+		DocumentManager = brackets.getModule("document/DocumentManager");
 
 	var cm = null,
-        cd = null,
-        breakpoints = [],
-        _nodeDebuggerDomain,
+		cd = null,
+		breakpoints = [],
+		_nodeDebuggerDomain,
 		gutterName = 'node-debugger-bp-gutter';
 
-    /*
-    * Sets the CodeMirror instance for the active editor
-    */
+	/*
+	* Sets the CodeMirror instance for the active editor
+	*/
 	function _updateCm() {
 		var editor = EditorManager.getActiveEditor();
 
-        if (!editor || !editor._codeMirror) {
-            return;
-        }
+		if (!editor || !editor._codeMirror) {
+			return;
+		}
 
 		cm = editor._codeMirror;
 
-        //Get the path to the current file as well
-        var _cd = DocumentManager.getCurrentDocument();
-        if(_cd) {
-            cd = _cd.file.fullPath;
-        }
+		//Get the path to the current file as well
+		var _cd = DocumentManager.getCurrentDocument();
+		if(_cd) {
+			cd = _cd.file.fullPath;
+		}
 
 	}
 
-    /*
-    * Set all gutters for the currentDocument
-    */
+	/*
+	* Set all gutters for the currentDocument
+	*/
 	function _updateGutters() {
 		if (!cm) { return; }
 
-        var gutters = cm.getOption("gutters").slice(0);
-        if (gutters.indexOf(gutterName) === -1) {
-            gutters.unshift(gutterName);
-            cm.setOption("gutters", gutters);
-            cm.on("gutterClick", gutterClick);
-        }
+		var gutters = cm.getOption("gutters").slice(0);
+		if (gutters.indexOf(gutterName) === -1) {
+			gutters.unshift(gutterName);
+			cm.setOption("gutters", gutters);
+			cm.on("gutterClick", gutterClick);
+		}
 
 		//Set all the gutters now
 		breakpoints.forEach(function(bp) {
@@ -59,7 +59,7 @@ define(function (require, exports) {
 					.addClass('breakpoint-gutter')
 					.html("●");
 
-            	bp.cm.setGutterMarker( bp.line, gutterName, $marker[0] );
+				bp.cm.setGutterMarker( bp.line, gutterName, $marker[0] );
 			}
 		});
 	}
@@ -68,116 +68,120 @@ define(function (require, exports) {
 	* remove all gutters from the current document
 	*/
 	function _clearGutters() {
-	  	if(!cm) { return; }
+		if(!cm) { return; }
 		var gutters = cm.getOption("gutters").slice(0),
-        io = gutters.indexOf(gutterName);
-        if (io !== -1) {
-            gutters.splice(io, 1);
-            cm.clearGutter(gutterName);
-            cm.setOption("gutters", gutters);
-            cm.off("gutterClick", gutterClick);
-        }
+		io = gutters.indexOf(gutterName);
+		if (io !== -1) {
+			gutters.splice(io, 1);
+			cm.clearGutter(gutterName);
+			cm.setOption("gutters", gutters);
+			cm.off("gutterClick", gutterClick);
+		}
 	}
 
-    /*
-    * Sets or removes Breakpoint at cliked line
-    *
-    * @param {CodeMirror} cm
-    * The CodeMirror instance
-    *
-    * @param {Number} n
-    * LineNumber
-    *
-    * @param {String} gutterId
-    */
+	/*
+	* Sets or removes Breakpoint at cliked line
+	*
+	* @param {CodeMirror} cm
+	* The CodeMirror instance
+	*
+	* @param {Number} n
+	* LineNumber
+	*
+	* @param {String} gutterId
+	*/
 	function gutterClick(cm, n, gutterId) {
 		if (gutterId !== gutterName && gutterId !== "CodeMirror-linenumbers") {
-            return;
-        }
+			return;
+		}
 
 		var info = cm.lineInfo(n);
 
 		if(info.gutterMarkers && info.gutterMarkers[gutterName]) {
-            var bp = _.find(breakpoints, function(obj) {
-                return obj.line === n && cd === obj.fullPath;
-            });
-            _nodeDebuggerDomain.exec("removeBreakpoint", bp.breakpoint);
-            cm.setGutterMarker( bp.line, gutterName, null );
-            var i = breakpoints.indexOf(bp);
-            breakpoints.splice(i, 1);
+			var bp = _.find(breakpoints, function(obj) {
+				return obj.line === n && cd === obj.fullPath;
+			});
+			_nodeDebuggerDomain.exec("removeBreakpoint", bp.breakpoint);
+			cm.setGutterMarker( bp.line, gutterName, null );
+			var i = breakpoints.indexOf(bp);
+			breakpoints.splice(i, 1);
 		} else {
-            //TODO Show warning if not connected
-            _nodeDebuggerDomain.exec("setBreakpoint", cd, n);
+			//TODO Show warning if not connected
+			_nodeDebuggerDomain.exec("setBreakpoint", cd, n);
 		}
 	}
 
-    /*
-    *   @param {NodeDomain} nodeDebuggerDomain
-    */
+	/*
+	*   @param {NodeDomain} nodeDebuggerDomain
+	*/
 	function init(nodeDebuggerDomain) {
-        _nodeDebuggerDomain = nodeDebuggerDomain;
+		_nodeDebuggerDomain = nodeDebuggerDomain;
 		_updateCm();
 		_updateGutters();
 	}
 
-    /* Sets the breakpoint gutter
-    *
-    * @param {breakpoint} bp
-    * bp as object like the V8 Debugger sends it
-    *
-    */
-    function addBreakpoint(bp) {
-        //If this one of the reconnect BP don't add it
-        var exist = _.find(breakpoints, function(obj) {
-            return obj.line === bp.line && obj.fullPath === bp.fullPath;
-        });
-        if(!exist) {
-            bp.cm = cm;
-            breakpoints.push(bp);
+	/* Sets the breakpoint gutter
+	*
+	* @param {breakpoint} bp
+	* bp as object like the V8 Debugger sends it
+	*
+	*/
+	function addBreakpoint(bp) {
+		//If this one of the reconnect BP don't add it
+		var exist = _.find(breakpoints, function(obj) {
+			return obj.line === bp.line && obj.fullPath === bp.fullPath;
+		});
+		if(!exist) {
+			bp.cm = cm;
+			breakpoints.push(bp);
 
-            var $marker = $("<div>")
-                    .addClass('breakpoint-gutter')
-                    .html("●");
+			var $marker = $("<div>")
+					.addClass('breakpoint-gutter')
+					.html("●");
 
-            bp.cm.setGutterMarker( bp.line, gutterName, $marker[0] );
-        }
-    }
+			bp.cm.setGutterMarker( bp.line, gutterName, $marker[0] );
+		}
+	}
 
-    /*
-    * Removes all Breakpoints
-    */
-    function removeAllBreakpoints() {
+	/*
+	* Removes all Breakpoints
+	*/
+	function removeAllBreakpoints() {
 		_clearGutters();
-        //Delete all
-        breakpoints = [];
-    }
+		//And actually remove the breakpoints when the debugger is running
+		breakpoints.forEach(function(bp) {
+			_nodeDebuggerDomain.exec("removeBreakpoint", bp.breakpoint);
+		});
+		//Delete all
+		breakpoints = [];
+	}
 
-    /*
-    * Call on connect
-    * Set all breakpoints if there are any
-    * Remove all gutters and request a list of breakpoints
-    * to make sure we're consistent
-    */
-    function setAllBreakpoints() {
-        if(breakpoints.length > 0) {
-            breakpoints.forEach(function(bp) {
-                _nodeDebuggerDomain.exec("setBreakpoint", bp.fullPath, bp.line);
-            });
-            //NOTE: Reload all Breakpoints?
-            //Request list of actual set breakpoints
-            //_nodeDebuggerDomain.exec("getBreakpoints");
-        }
-    }
+	/*
+	* Call on connect
+	* Set all breakpoints if there are any
+	* Remove all gutters and request a list of breakpoints
+	* to make sure we're consistent
+	*/
+	function setAllBreakpoints() {
+		if(breakpoints.length > 0) {
+			breakpoints.forEach(function(bp) {
+				_nodeDebuggerDomain.exec("setBreakpoint", bp.fullPath, bp.line);
+			});
+			//NOTE: Reload all Breakpoints?
+			//Request list of actual set breakpoints
+			//_nodeDebuggerDomain.exec("getBreakpoints");
+		}
+	}
 
-    $(DocumentManager).on("currentDocumentChange", function () {
-        _clearGutters();
+	$(DocumentManager).on("currentDocumentChange", function () {
+		_clearGutters();
 		_updateCm();
-        _updateGutters();
-    });
+		_updateGutters();
+	});
 
 
 	exports.init = init;
-    exports.addBreakpoint = addBreakpoint;
-    exports.setAllBreakpoints = setAllBreakpoints;
-    exports.removeAllBreakpoints = removeAllBreakpoints;
+	exports.addBreakpoint = addBreakpoint;
+	exports.setAllBreakpoints = setAllBreakpoints;
+	exports.removeAllBreakpoints = removeAllBreakpoints;
 });
--- src/changelogDialog.js
+++ src/changelogDialog.js
@@ -1,29 +1,29 @@
 /*global define, brackets, $, Mustache */
 define(function (require, exports, module) {
-    "use strict";
+	"use strict";
 
-    var Dialogs                    = brackets.getModule("widgets/Dialogs"),
-        FileSystem                 = brackets.getModule("filesystem/FileSystem"),
-        FileUtils                  = brackets.getModule("file/FileUtils"),
-        ExtensionUtils             = brackets.getModule("utils/ExtensionUtils"),
-        changelogDialogTemplate    = require("text!../assets/changelog-dialog.html"),
-        marked                     = require("./thirdparty/marked");
+	var Dialogs                    = brackets.getModule("widgets/Dialogs"),
+		FileSystem                 = brackets.getModule("filesystem/FileSystem"),
+		FileUtils                  = brackets.getModule("file/FileUtils"),
+		ExtensionUtils             = brackets.getModule("utils/ExtensionUtils"),
+		changelogDialogTemplate    = require("text!../assets/changelog-dialog.html"),
+		marked                     = require("./thirdparty/marked");
 
-    var dialog;
+	var dialog;
 
-    exports.show = function () {
-        var compiledTemplate = Mustache.render(changelogDialogTemplate);
+	exports.show = function () {
+		var compiledTemplate = Mustache.render(changelogDialogTemplate);
 
-        var path = ExtensionUtils.getModulePath(module, '../CHANGELOG.md');
+		var path = ExtensionUtils.getModulePath(module, '../CHANGELOG.md');
 
-        FileUtils.readAsText(FileSystem.getFileForPath(path)).done(function (content) {
-            content = marked(content, {
-                gfm: true,
-                breaks: true
-            });
-            dialog = Dialogs.showModalDialogUsingTemplate(compiledTemplate);
-            $("#node-debugger-changelog", dialog.getElement()).html(content);
-        });
-    };
+		FileUtils.readAsText(FileSystem.getFileForPath(path)).done(function (content) {
+			content = marked(content, {
+				gfm: true,
+				breaks: true
+			});
+			dialog = Dialogs.showModalDialogUsingTemplate(compiledTemplate);
+			$("#node-debugger-changelog", dialog.getElement()).html(content);
+		});
+	};
 
 });
